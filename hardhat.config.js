86076425de6bbdbda37772322c5ef3abe47672e4
hallenDev <hallendev0531@gmail.com>
2022-07-08 11:46:25 +0900

    update hardhat.config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,48 @@
+require('dotenv').config();
+require("@nomiclabs/hardhat-waffle");
+require('hardhat-deploy');
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: {
-    version: '0.8.1',
+    version: '0.8.13',
     settings: {
       optimizer: {
         enabled: true,
         runs: 200
       }
     }
+  },
+  networks: {
+    avalanche: {
+      chainId: 43114,
+      url: process.env.AVALANCHE_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    fuji: {
+      chainId: 43113,
+      url: process.env.FUJI_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    BSC: {
+      chainId: 56,
+      url: process.env.BSC_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    BST_TEST: {
+      chainId: 97,
+      url: process.env.BSC_TEST_RPC_URL,
+      accounts:[process.env.PRIVATE_KEY]
+    }
+  },
+  namedAccounts: {
+    account0: 0
+  },
+  etherscan: {
+    // apiKey: process.env.SNOWTRACE_API_KEY // avalanche
+    // apiKey: process.env.ETHERSCAN_API_KEY  // ethereum
+    apiKey: process.env.BSCSCAN_API_KEY  //bsc
   }
 };
